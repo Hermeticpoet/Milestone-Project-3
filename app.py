40211dd786cca7c677be0e560aba192e43825014
= <=>
2019-07-23 13:23:43 +0000

    created a forms.py file with registration & login classes then created routes to both

--- app.py
+++ app.py
@@ -2,24 +2,39 @@ import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = '9039e1a76014f85ad2fea4415a793bd8'
 app.config["MONGO_DBNAME"] = 'recipesApp'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
+login = LoginManager(app)
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
     
 @app.route("/get_recipes")
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
     
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register" form=form)
+    
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login" form=form)
     
 
 if __name__ == '__main__':
