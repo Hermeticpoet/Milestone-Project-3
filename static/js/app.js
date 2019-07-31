63d300a8f41f843b5781cdf530d617755ba4c55c
= <=>
2019-07-31 10:54:51 +0000

    changed the formSelect() function that was causing issue to material_select()

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
     $(".dropdown-trigger").dropdown({ hover: false });
-    // $('select').formSelect();
     $('select').material_select();
 });
