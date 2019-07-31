192eda9d404b6b8e16d15168a2231108eef22449
= <=>
2019-07-31 10:53:18 +0000

    Corrected dropdown options for add_recipe page so it now works

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
     $(".dropdown-trigger").dropdown({ hover: false });
+    // $('select').formSelect();
     $('select').material_select();
 });
