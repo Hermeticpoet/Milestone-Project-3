5a918fa875044b0726e2e302941955438f0247b1
= <=>
2019-08-07 14:45:31 +0000

    Removed dropdown trigger as no longer needed

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
     $('.collapsible').collapsible();
-    $(".dropdown-trigger").dropdown({ hover: false });
     $('select').material_select();
 });
